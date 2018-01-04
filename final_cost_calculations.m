5e9537a09319eb88d67e1d4392ba52a4676c3c9e
ajpimm <andrewpimm@gmail.com>
2018-01-04 14:06:29 +0000

    Update final_cost_calculations.m

--- final_cost_calculations.m
+++ final_cost_calculations.m
@@ -15,7 +15,7 @@ pipe_flows = get_pipe_flows(edges,source_flows);
 junction_coords = all_coords(num_sources+1:end-1,:);
 [total_cost,pipe_costs,pipe_lengths,pipe_diameters]...
     = cost_fun_w_mass_flows(reshape(junction_coords',[],1),edges,reshape(source_coords',[],1),...
-    reshape(reservoir_coords',[],1),pipe_flows,1);
+    reshape(reservoir_coords',[],1),pipe_flows,'1');
 
 total_cost = total_cost + 1244724 + 111907;
 
